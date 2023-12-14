c7e2fb7dbe542f635268e4d5039044a420806e08
exploitenomah <iksploiting@gmail.com>
2023-12-14 15:31:00 +0100

    create user controllers

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,7 @@
 var express = require('express');
-const { create, findOne, updateOne, deleteOne } = require("../controllers/user");
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/:id', async function(req, res, next) {
-  res.status(200).json({data: await deleteOne({_id:req.params.id})})
 });
 
 module.exports = router;
