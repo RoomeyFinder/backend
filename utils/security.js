47fd6dfdec85255c96637712da401e339df5e249
exploitenomah <iksploiting@gmail.com>
2023-12-15 19:00:18 +0100

    create jwt helper

--- utils/security.js
+++ utils/security.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 module.exports.hashValue = async function (value) {
   const salt = await bcrypt.genSalt(10)
@@ -6,4 +7,8 @@ module.exports.hashValue = async function (value) {
 }
 module.exports.compareValueToHash = async function (value, hash) {
   return await bcrypt.compare(value, hash)
+}
+
+module.exports.createJWT = function(payload){
+  return jwt.sign(payload, process.env.JWT_SECRET)
 }
