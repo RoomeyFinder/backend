571b06f93cac329c77c26eb1f203af7a0d4a0914
exploitenomah <iksploiting@gmail.com>
2023-12-18 04:58:00 +0100

    fix route-handlers, create verify-email handler

--- route-handlers/user.js
+++ route-handlers/user.js
@@ -9,8 +9,22 @@ module.exports.signup = routeTryCatcher(async function(req, res, next){
   await user.save()
   req.response = {
     user,
-    message: "success",
-    status: 201
+    status: "success",
+    statusCode: 201
+  }
+  return next()
+})
+
+module.exports.verifyEmail = routeTryCatcher(async function (req, res, next) {
+  let user = await findOne({ _id: req.params.id, emailVerificationToken: req.params.emailVerificationToken })
+  if (!user) return next(new CustomError("Not allowed!", 403))
+  user.emailVerificationToken = undefined
+  user.isEmailVerified = true
+  await user.save()
+  req.response = {
+    user,
+    status: "success",
+    statusCode: 200
   }
   return next()
 })
@@ -20,7 +34,7 @@ module.exports.login = routeTryCatcher(async function(req, res, next){
   const user = await findOne({ $or: [{ email: emailOrUserName }, { userName: emailOrUserName }]})
   if(!user) return next(new CustomError("Invalid credentials!", 400))
   const isMatchingPassword = await compareValueToHash(password, user.password)
-  if(!isMatchingPassword) return  next(new CustomError("Invalid credentials", 400))
+  if (isMatchingPassword === false) return next(new CustomError("Invalid credentials", 400))
   if(!user.isEmailVerified){
     await sendVerificationEmail(user)
     await user.save()
@@ -29,21 +43,21 @@ module.exports.login = routeTryCatcher(async function(req, res, next){
   const token = createJWT({ _id: user._id })
   delete user.password
   req.response = {
-    status: 200,
+    statusCode: 200,
     user,
-    token
+    token,
+    status: "success"
   }
   return next()
 })
 
 module.exports.getUser = routeTryCatcher(async function (req, res, next) {
-  console.log(req.params)
   const user = await findOne({ _id: req.params.id })
   delete user.password
   req.response = {
-    status: 200,
+    statusCode: 200,
     user,
-    message: "success"
+    status: "success"
   }
   return next()
 })
@@ -64,8 +78,8 @@ module.exports.updateUser = routeTryCatcher(async function(req, res, next){
   })
   req.response = {
     user,
-    status: 200,
-    message: "success"
+    statusCode: 200,
+    status: "success"
   }
   return next()
 })
@@ -73,9 +87,9 @@ module.exports.updateUser = routeTryCatcher(async function(req, res, next){
 module.exports.getMultipleUsers = routeTryCatcher(async function (req, res, next) {
   const users = await findMany(req.query)
   req.response = {
-    status: 200,
+    statusCode: 200,
     users,
-    message: "success"
+    status: "success"
   }
   return next()
 })
@@ -83,9 +97,9 @@ module.exports.deleteAccount = routeTryCatcher(async function (req, res, next) {
   if(req.user._id.toString() !== req.params.id) return next("Not allowed!", 403)
   const user = await deleteOne({ _id: req.user._id.toString() })
   req.response = {
-    status: 200,
+    statusCode: 200,
     user,
-    message: "success"
+    status: "success"
   }
   return next()
 })
