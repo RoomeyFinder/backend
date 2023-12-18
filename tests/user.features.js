d696b31174e13b00cf70597729a0ecdacef3ecb4
exploitenomah <iksploiting@gmail.com>
2023-12-18 05:00:43 +0100

    tests for users api

---
+++ tests/user.features.js
@@ -0,0 +1,14 @@
+
+
+module.exports.signupData = {
+  "firstName": "exploit",
+  "lastName": "enomah",
+  "email": "exploitenomah@gmail.com",
+  "password": "dev1234",
+  "longitude": "0",
+  "latitude": "0",
+  "countryCode": "234",
+  "phoneNumber": "9012345678",
+  "gender": "female",
+  "dob": "2001-03-27T23:15:30.000Z"
+}
