0c1f2aa4ff7f124b72ce0ab93a4a14a2a4207200
exploitenomah <iksploiting@gmail.com>
2023-12-19 19:00:32 +0100

    write tests for unique username

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,7 +2,9 @@ const request = require("supertest")
 const app = require("../app")
 const { expect } = require("@jest/globals")
 const { connectDB, dropDBAndDisconnect, dropCollection } = require("./db");
-const { signup, login, verifyEmail, } = require("./auth.utils");
+const { signupDefaultUser, login, verifyEmail, } = require("./auth.utils");
+const { seedUsers } = require("./seed");
+const users = require("./seeds/users.json")
 
 require("./db")
 require("dotenv").config();
@@ -18,14 +20,14 @@ afterAll(async () => {
   server?.close()
 })
 
-describe("User Signup, Email Verification, Login and CRUD Operations", () => {
-  afterAll(() => {
-    dropCollection("users")
+describe("User signup, Email Verification, Login and CRUD Operations", () => {
+  afterAll(async () => {
+    await dropCollection("users")
   })
   let user = null
   let token = null
   it("should signup user: POST /api/v1/users", async () => {
-    const response = await signup(server)
+    const response = await signupDefaultUser(server)
     const { body } = await response
     user = body.user
     expect(body.statusCode).toBe(201)
@@ -78,9 +80,12 @@ describe("User Signup, Email Verification, Login and CRUD Operations", () => {
 
 
 describe("User Schema Validation And Auto Modification Validation", () => {
+  afterAll(async () => {
+    await dropCollection("users")
+  })
   let user, token
   it("Pets and hasPets in tandem: Does not allow truthy value for hasPets field if pets.length is < 1 or if hasPets is falsy and pets.length > 0", async () => {
-    const { body } = await signup(server)
+    const { body } = await signupDefaultUser(server)
     user = body.user
     await verifyEmail(server)(user._id, user.emailVerificationToken)
     const response = await login(server)
@@ -140,5 +145,29 @@ describe("User Schema Validation And Auto Modification Validation", () => {
     const updatedUser = updatedUserResponse.body.user
     expect(updatedUser.isProfileComplete).toBeTruthy()
     expect(updatedUser.allergies.length).toBeLessThanOrEqual(1)
+  })
+})
+
+describe("Unique username and email", () => {
+  afterAll(async () => {
+    await dropCollection("users")
+  })
+  let user
+  it("Does not allow multiple users with the same email", async () => {
+    const user1Response = await signupDefaultUser(server)
+    user = await user1Response.body.user
+    const user2Response = await user1Response.status === 201 && await signupDefaultUser(server)
+    expect(user1Response.status).toBe(201)
+    expect(user2Response.status) .toBe(400)
+  })
+  it("Does not allow multiple users with the same username", async () => {
+    const users = await seedUsers(1)
+    await verifyEmail(server)(await user._id, await user.emailVerificationToken)
+    const loginResponse = await login(server)
+    await request(server)
+      .put(`/api/v1/users/${user._id}`)
+      .set("Authorization", `Bearer ${loginResponse.body.token}`)
+      .send({ userName: users[0].userName })
+      .expect(400)
   })
 })
