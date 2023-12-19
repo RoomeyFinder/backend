6208dfca879290165699629402c40905e6cd4165
exploitenomah <iksploiting@gmail.com>
2023-12-19 15:34:29 +0100

    rename

---
+++ tests/auth.utils.js
@@ -0,0 +1,30 @@
+
+const request = require("supertest")
+const app = require("../app");
+const { signupData } = require("./user.features");
+
+module.exports.signup = async (server) => {
+  return await request(server)
+    .post("/api/v1/users")
+    .send(signupData)
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+}
+
+module.exports.login = async (server) => {
+  return await request(server)
+    .post("/api/v1/users/login")
+    .send({
+      emailOrUserName: signupData.email,
+      password: signupData.password,
+    })
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+}
+
+module.exports.verifyEmail = (server) => async (userId, emailVerificationToken) => {
+  return await request(server)
+    .get(`/api/v1/users/verify-email/${userId}/${emailVerificationToken}`)
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+}
