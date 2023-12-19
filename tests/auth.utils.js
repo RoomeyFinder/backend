4e5ebc1b6a6433726dc8f04dbac31f5fc625d688
exploitenomah <iksploiting@gmail.com>
2023-12-19 18:59:32 +0100

    fix variable names

--- tests/auth.utils.js
+++ tests/auth.utils.js
@@ -3,10 +3,18 @@ const request = require("supertest")
 const app = require("../app");
 const { signupData } = require("./user.features");
 
-module.exports.signup = async (server) => {
+module.exports.signupDefaultUser = async (server) => {
   return await request(server)
     .post("/api/v1/users")
     .send(signupData)
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+}
+
+module.exports.signupUser = async (data) => {
+  return await request(app)
+    .post("/api/v1/users")
+    .send(data)
     .set("Accept", "application/json")
     .expect("Content-Type", /json/)
 }
