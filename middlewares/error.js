72223bc86a066f4388f786c44f96958abad46555
exploitenomah <iksploiting@gmail.com>
2023-12-19 15:16:33 +0100

    rename variables

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,5 @@
 const { handleValidationError, handleTypeError, handleCastError, handleDuplicateKeyError } = require("../utils/errorFormatters")
-const throwDevError = (err, res) => {
+const sendDevErrorResponse = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
@@ -8,7 +8,7 @@ const throwDevError = (err, res) => {
     statusCode: err.statusCode,
   })
 }
-const throwProdError = (err, res) => {
+const sendProductionErrorResponse = (err, res) => {
   console.error(err)
   err.isOperational
     ? res.status(err.statusCode).json({
@@ -26,14 +26,14 @@ const throwProdError = (err, res) => {
 module.exports = (err, req, res, next) => {
   err.status = err.status || "error"
   err.statusCode = err.statusCode || 500
-  if (process.env.NODE_ENV === "dev") {
-    throwDevError(err, res)
+  if (process.env.NODE_ENV !== "prod") {
+    sendDevErrorResponse(err, res)
   } else {
     let error = err
     if (err.name === "ValidationError") error = handleValidationError(err)
     if (err.code === 11000) error = handleDuplicateKeyError(err)
     if (err.name === "CastError") error = handleCastError(err)
     if (err.name === "TypeError") error = handleTypeError(err)
-    throwProdError(error, res)
+    sendProductionErrorResponse(error, res)
   }
 }
