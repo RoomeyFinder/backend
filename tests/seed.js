720e7b90613be5f3f534636ca2cb3c6b680f6008
exploitenomah <iksploiting@gmail.com>
2024-01-05 03:39:09 +0100

    refactor

--- tests/seed.js
+++ tests/seed.js
@@ -1,12 +1,20 @@
 const User = require("../models/user")
 const { signupUser } = require("./auth.utils")
 const usersJson = require("./seeds/users.json")
+const listingsJson = require("./seeds/listings.json")
 
-module.exports.seedUsers = async function(count = 5){
+module.exports.seedUsers = async function(server, count = 5){
   let seedUsers = usersJson
-  if (count < seedUsers.length) seedUsers = seedUsers.filter((it, idx) => idx < count)
+  if (count < seedUsers.length) seedUsers = seedUsers.filter((_, idx) => idx < count)
   const users = await Promise.all(seedUsers.map(async it => { 
-    return (await signupUser(it)).body.user
+    return await signupUser(server)(it)
   }))
   return users
+}
+
+
+module.exports.seedListings = async function (server, count) {
+  return async function (userId, photos){
+    
+  }
 }
