c405312bde9209b7cfad3dddd1500ab3eadd2772
exploitenomah <iksploiting@gmail.com>
2024-01-05 12:08:09 +0100

    create middlewares for file handling

---
+++ middlewares/cloudinary.js
@@ -0,0 +1,42 @@
+const { v2: cloudinary } = require("cloudinary")
+const DatauriParser = require("datauri/parser")
+const CustomError = require("../utils/error")
+const parser = new DatauriParser()
+cloudinary.config({
+  cloud_name: process.env.CLOUDNARY_NAME,
+  api_key: process.env.CLOUDNARY_API_KEY,
+  api_secret: process.env.CLOUDNARY_API_SECRET,
+})
+module.exports = async (req, res, next) => {
+  if (!req.files || req.files.length === 0) return next()
+  const done = await Promise.allSettled(
+      req.files.map(async (file) => {
+      if (file) {
+        const fileExt = `.${file.mimetype.split("/")[1]}`
+        const formatted = parser.format(fileExt, file.buffer)
+        let response
+        try {
+          response = await cloudinary.uploader.upload(formatted.content, {
+            public_id: `${file.fieldname}_${Date.now()}`,
+          })
+        } catch (err) {
+          console.log(err.message)
+          next(new CustomError(err.message), 500)
+        }
+        if (!response) {
+          return next(new CustomError("Something went wrong!", 500))
+        }
+        if(req.imagesPath === undefined)
+          req.body[file.fieldname] = response.secure_url
+        const { asset_id, public_id, width, height,secure_url, etag, created_at } = response
+        return {
+          asset_id, public_id, width, height, secure_url, etag, created_at
+        }
+      }
+    })
+  )
+  if (done.length !== req.files.length)
+    return next(new CustomError("Unable to upload some files!", 500))
+  req.body[req.imagesPath] = done.map(it => it.value)
+  next()
+}
