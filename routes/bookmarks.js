5f32ded41ac94ead96895b96773195ba43b1dc7e
exploitenomah <iksploiting@gmail.com>
2024-01-06 13:13:06 +0100

    create bookmarks route

---
+++ routes/bookmarks.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const { protectRoute } = require("../middlewares/auth");
+const { sendResponse } = require("../utils/routes");
+const { getMultipleBookmarks, createBookmark, getBookmark, deleteBookmark } = require("../route-handlers/bookmark");
+var router = express.Router();
+
+router.get("/",  protectRoute, getMultipleBookmarks, sendResponse)
+router.post("/", protectRoute, createBookmark, sendResponse)
+router.get("/:id", protectRoute, getBookmark, sendResponse)
+router.delete("/:id", protectRoute, deleteBookmark, sendResponse)
+
+module.exports = router
