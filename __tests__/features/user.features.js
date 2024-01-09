865d78b26d6c4839c499181e987840e2971d22a7
exploitenomah <iksploiting@gmail.com>
2024-01-09 14:45:41 +0100

    refactor test suites

--- __tests__/features/user.features.js
+++ __tests__/features/user.features.js
@@ -12,3 +12,23 @@ module.exports.signupData = {
   "gender": "female",
   "dob": "2001-03-27T23:15:30.000Z"
 }
+
+module.exports.updateData = {
+  phoneNumber: "123456",
+  countryCode: "234",
+  isEmailVerified: true,
+  dob: "2001-12-19T13:39:07.834Z", 
+  about: "about",
+  stateOfOrigin: "Rivers",
+  countryOfOrigin: "Nigeria",
+  earliestMoveDate: new Date(Date.now()).toDateString(), lookingFor: "room",
+  gender: "female",
+  currentCity: "current city",
+  currentState: "current state",
+  currentAddress: "current address",
+  targetCity: "target city",
+  targetState: "target location",
+  hasPets: false, pets: [], hasAllergies: false, 
+  allergies: [], budget: 300, isStudent: true, 
+  school: "uniport", major: "Computer science",
+}
