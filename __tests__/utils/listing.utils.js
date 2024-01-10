6f51e67ee6879ea8562804c39c79015263a6a954
exploitenomah <iksploiting@gmail.com>
2024-01-10 16:04:49 +0100

    refactor

--- __tests__/utils/listing.utils.js
+++ __tests__/utils/listing.utils.js
@@ -4,12 +4,12 @@ const { getPhotos, sendMultipartBodyRequest } = require(".")
 
 
 module.exports.createListing = function (server, token) {
-  return async function (userId, fields = null, photosCount = 3) {
+  return async function (userId, fields = null, photosCount = 3,) {
     const photos = getPhotos(photosCount)
     const send = async (server, data, token) =>  
       await sendMultipartBodyRequest(server, "post", "/api/v1/listings", data, token)
     if(!fields)
-      return await send(server, { ...listingsJson[0], owner: userId, photos }, token)
+      return await send(server, { ...listingsJson[0], owner: userId, photos, isDraft: false }, token)
     else {
       let listing = {}
       for (let idx = 0; idx < fields.length; idx++) {
