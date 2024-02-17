ffb312143674b3a22cbc5cd6ffdb77afde382d6e
exploitenomah <iksploiting@gmail.com>
2024-02-17 20:16:02 +0100

    add status code to response body

--- route-handlers/feedback.js
+++ route-handlers/feedback.js
@@ -5,6 +5,7 @@ module.exports.handlePost = routeTryCatcher(async (req, res, next) => {
   const feedback = await createFeedback(req.body)
   res.status(200).json({
     feedback,
-    message: "Thank you for your message! We'll get in touch shortly."
+    message: "Thank you for your message! We'll get in touch shortly.",
+    statusCode: 201
   })
 })
